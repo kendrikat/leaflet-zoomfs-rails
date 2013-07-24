6524bd936e85d10e379e59b1583f8513cf8fe9e7
Klaas Endrikat <klaas.endrikat@googlemail.com>
2013-07-24 21:04:22 +0200

    init

---
+++ lib/leaflet-zoomfs-rails.rb
@@ -0,0 +1,9 @@
+require "leaflet-zoomfs-rails/version"
+
+module Leaflet
+  module Zoomfs
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/leaflet-zoomfs-rails/version.rb
@@ -0,0 +1,7 @@
+module Leaflet
+  module Zoomfs
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
